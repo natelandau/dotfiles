c07904503d471ff59974eb72dc6e37ee19530c9a
Nathaniel Landau <nate@natelandau.com>
2017-01-24 21:17:51 -0500

    better extract function

--- config/bash/files.bash
+++ config/bash/files.bash
@@ -20,41 +20,59 @@ function extract() {
   local foldername
   local fullpath
   local didfolderexist
-
-  # Extract archives - use: extract <file>
-  # Based on http://dotfiles.org/~pseup/.bashrc
-  if [ -f "$1" ] ; then
-    filename="$(basename "$1")"
-    foldername="${filename%%.*}"
-    fullpath=$(perl -e 'use Cwd "abs_path";print abs_path(shift)' "$1")
-    didfolderexist=false
-    if [ -d "$foldername" ]; then
-      didfolderexist=true
-      read -p "$foldername already exists, do you want to overwrite it? (y/n) " -n 1
-      echo
-      if [[ $REPLY =~ ^[Nn]$ ]]; then
-        return
-      fi
-    fi
-    mkdir -p "$foldername" && cd "$foldername"
-    case $1 in
-      *.tar.bz2) tar xjf "$fullpath" ;;
-      *.tar.gz) tar xzf "$fullpath" ;;
-      *.tar.xz) tar Jxvf "$fullpath" ;;
-      *.tar.Z) tar xzf "$fullpath" ;;
-      *.tar) tar xf "$fullpath" ;;
-      *.taz) tar xzf "$fullpath" ;;
-      *.tb2) tar xjf "$fullpath" ;;
-      *.tbz) tar xjf "$fullpath" ;;
-      *.tbz2) tar xjf "$fullpath" ;;
-      *.tgz) tar xzf "$fullpath" ;;
-      *.txz) tar Jxvf "$fullpath" ;;
-      *.zip) unzip "$fullpath" ;;
-      *) echo "'$1' cannot be extracted via extract()" && cd .. && ! $didfolderexist && rm -r "$foldername" ;;
+  local opt
+  local OPTIND=1
+  while getopts "hv" opt; do
+    case "$opt" in
+      h)
+        cat <<End-Of-Usage
+Usage: ${FUNCNAME[0]} [option] <archives>
+  options:
+    -h  show this message and exit
+    -v  verbosely list files processed
+End-Of-Usage
+    return
+      ;;
+    v)
+      local -r verbose='v'
+      ;;
+    ?)
+      extract -h >&2
+      return 1
+      ;;
     esac
-  else
-    echo "'$1' is not a valid file"
-  fi
+  done
+  shift $((OPTIND-1))
+
+  [ $# -eq 0 ] && extract -h && return 1
+  while [ $# -gt 0 ]; do
+    if [ -f "$1" ]; then
+      case "$1" in
+        *.tar.bz2|*.tbz|*.tbz2) tar "x${verbose}jf" "$1" ;;
+        *.tar.gz|*.tgz) tar "x${verbose}zf" "$1" ;;
+        *.tar.xz) xz --decompress "$1"; set -- "$@" "${1:0:-3}" ;;
+        *.tar.Z) uncompress "$1"; set -- "$@" "${1:0:-2}" ;;
+        *.bz2) bunzip2 "$1" ;;
+        *.deb) dpkg-deb -x${verbose} "$1" "${1:0:-4}" ;;
+        *.pax.gz) gunzip "$1"; set -- "$@" "${1:0:-3}" ;;
+        *.gz) gunzip "$1" ;;
+        *.pax) pax -r -f "$1" ;;
+        *.pkg) pkgutil --expand "$1" "${1:0:-4}" ;;
+        *.rar) unrar x "$1" ;;
+        *.rpm) rpm2cpio "$1" | cpio -idm${verbose} ;;
+        *.tar) tar "x${verbose}f" "$1" ;;
+        *.txz) mv "$1" "${1:0:-4}.tar.xz"; set -- "$@" "${1:0:-4}.tar.xz" ;;
+        *.xz) xz --decompress "$1" ;;
+        *.zip|*.war|*.jar) unzip "$1" ;;
+        *.Z) uncompress "$1" ;;
+        *.7z) 7za x "$1" ;;
+        *) echo "'$1' cannot be extracted via extract" >&2;;
+      esac
+    else
+      echo "extract: '$1' is not a valid file" >&2
+    fi
+    shift
+  done
 }
 
 
