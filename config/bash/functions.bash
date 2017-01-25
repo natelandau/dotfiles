665ee0f07fccc171b8e23fed6f73d256aa56447d
Nathaniel Landau <nate@natelandau.com>
2017-01-24 21:18:07 -0500

    explain added

--- config/bash/functions.bash
+++ config/bash/functions.bash
@@ -215,3 +215,22 @@ function repeat() {
       eval "$@";
   done
 }
+
+function explain () {
+  # about 'explain any bash command via mankier.com manpage API'
+  # example '$ explain                # interactive mode. Type commands to explain in REPL'
+  # example '$ explain 'cmd -o | ...' # one quoted command to explain it.'
+
+  if [ "$#" -eq 0 ]; then
+    while read  -p "Command: " cmd; do
+      curl -Gs "https://www.mankier.com/api/explain/?cols="$(tput cols) --data-urlencode "q=$cmd"
+    done
+    echo "Bye!"
+  elif [ "$#" -eq 1 ]; then
+    curl -Gs "https://www.mankier.com/api/explain/?cols="$(tput cols) --data-urlencode "q=$1"
+  else
+    echo "Usage"
+    echo "explain                  interactive mode."
+    echo "explain 'cmd -o | ...'   one quoted command to explain it."
+  fi
+}
