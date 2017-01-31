2222f26b12da7ba810bff8a1ae2c9d07724bd939
Nathaniel Landau <nate@natelandau.com>
2017-01-31 16:08:59 -0500

    Better iTerm config
    - Use Meslo font
    - renamed color profile to 'dotfiles'
    - updated plist preferences
    - rewrote config script to move all the above to ~/Library

--- lib/configure/02-iTerm.sh
+++ lib/configure/02-iTerm.sh
@@ -1,13 +1,47 @@
 
 info "Configuring iTerm..."
 
- if ! [ -e /Applications/iTerm.app ]; then
+if ! [ -e /Applications/iTerm.app ]; then
   warning "Could not find iTerm.app. Please install iTerm and run this again."
 else
 
-  execute "defaults write com.apple.terminal StringEncodings -array 4" "Only use UTF-8 in Terminal.app"
+  # iTerm config files location
+  iTermConfig="${baseDir}/config/iTerm"
 
-  execute "defaults write com.googlecode.iterm2 PromptOnQuit -bool false" "Don’t display the annoying prompt when quitting iTerm"
+  if [ -d "${iTermConfig}" ]; then
 
-  execute "open ${baseDir}/config/iTerm/themes/nate.itermcolors" "Installing preferred color scheme"
+    # 1. Copy fonts
+    fontLocation="$HOME/Library/Fonts"
+    for font in $iTermConfig/fonts/**/*.otf; do
+      baseFontName=$(basename "$font")
+      destFile="${fontLocation}/${baseFontName}"
+      if [ ! -e "$destFile" ]; then
+        execute "cp $font $destFile"
+      fi
+    done
+
+    # 2. symlink preferences
+    sourceFile="${iTermConfig}/com.googlecode.iterm2.plist"
+    destFile="$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+
+    if [ ! -e "$destFile" ]; then
+      execute "cp $sourceFile $destFile" "cp $sourceFile → $destFile"
+    elif [ -h "$destFile" ]; then
+      originalFile=$(locateSourceFile "$destFile")
+      backupOriginalFile "$originalFile"
+      if ! $dryrun; then rm -rf "$destFile"; fi
+      execute "cp $sourceFile $destFile" "cp $sourceFile → $destFile"
+    elif [ -e "$destFile" ]; then
+      backupOriginalFile "$destFile"
+      if ! $dryrun; then rm -rf "$destFile"; fi
+      execute "cp $sourceFile $destFile" "cp $sourceFile → $destFile"
+    else
+      warning "Error linking: $sourceFile → $destFile"
+    fi
+
+    #3 Install preferred colorscheme
+    execute "open ${baseDir}/config/iTerm/themes/dotfiles.itermcolors" "Installing preferred color scheme"
+  else
+    warning "Couldn't find iTerm configuration files"
+  fi
 fi
