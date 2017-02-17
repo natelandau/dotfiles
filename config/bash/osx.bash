2b4fb0bde1971b27c712ed454153f538e31f64a9
Nathaniel Landau <nate@natelandau.com>
2017-02-17 17:50:42 -0500

    add finderpath

--- config/bash/osx.bash
+++ config/bash/osx.bash
@@ -70,11 +70,11 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
     local finderPath
 
-    finderPath=$(osascript -e 'tell application "Finder"'\
-      -e "if (${1-1} <= (count Finder windows)) then"\
-      -e "get POSIX path of (target of window ${1-1} as alias)"\
+    finderPath=$(osascript -e 'tell application "Finder"' \
+      -e "if (${1-1} <= (count Finder windows)) then" \
+      -e "get POSIX path of (target of window ${1-1} as alias)" \
       -e 'else' \
-      -e 'get POSIX path of (desktop as alias)'\
+      -e 'get POSIX path of (desktop as alias)' \
       -e 'end if' \
       -e 'end tell';)
 
