ee373b60938121b0990864160cbb47d68f2921a2
Nathaniel Landau <nate@natelandau.com>
2017-04-21 21:58:30 -0400

    localized variables

--- lib/configure/05-dotfilesGitHooks.sh
+++ lib/configure/05-dotfilesGitHooks.sh
@@ -3,6 +3,8 @@
 _installGitHooks_() {
   info "Installing git hooks for this repository..."
 
+  local GITROOT
+  local hook
   GITROOT=$(git rev-parse --show-toplevel 2> /dev/null)
 
   if [ "${GITROOT}" == "" ]; then
@@ -11,7 +13,7 @@ _installGitHooks_() {
   fi
 
   # Location of hooks
-  hooksLocation="${baseDir}/.hooks"
+  local hooksLocation="${GITROOT}/.hooks"
 
   if ! [ -d "$hooksLocation" ]; then
     warning "Can't find hooks. Exiting."
@@ -21,8 +23,8 @@ _installGitHooks_() {
   for hook in ${hooksLocation}/*.sh; do
     hook="$(basename ${hook})"
 
-    sourceFile="${hooksLocation}/${hook}"
-    destFile="${GITROOT}/.git/hooks/${hook%.sh}"
+    local sourceFile="${hooksLocation}/${hook}"
+    local destFile="${GITROOT}/.git/hooks/${hook%.sh}"
 
     if [ -e "$destFile" ]; then
       _execute_ "rm $destFile"
@@ -30,11 +32,5 @@ _installGitHooks_() {
     _execute_ "ln -fs \"$sourceFile\" \"$destFile\"" "symlink $sourceFile → $destFile"
 
   done
-
-  unset sourceFile
-  unset destFile
-  unset hook
-  unset hooksLocation
-  unset GITROOT
 }
 _installGitHooks_
