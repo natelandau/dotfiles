c401f1e76c856776f73edb4c5a8afa251db1bf99
Nathaniel Landau <nate@natelandau.com>
2017-08-18 16:33:56 -0400

    Linux bootstrap scripts initial commit
      - Bats tests added
      - linux plugins added
      - install script and associated config file added

---
+++ bootstrap/lib/linux-plugins/02-install-gitFriendly.sh
@@ -0,0 +1,219 @@
+#!/usr/bin/env bash
+version="1.0.0"
+
+_mainScript_() {
+
+  if ! command -v pull &> /dev/null; then
+    _installGitFriendly_() {
+      info "Installing git-friendly...."
+
+      # github.com/jamiew/git-friendly
+      # the `push` command which copies the github compare URL to my clipboard is heaven
+      _execute_ "bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)"
+    }
+    _installGitFriendly_
+  else
+    success "git-friendly already installed"
+  fi
+}
+
+_trapCleanup_() {
+  echo ""
+  die "Exit trapped. In function: '${FUNCNAME[*]:1}'"
+}
+
+_safeExit_() {
+  trap - INT TERM EXIT
+  exit ${1:-0}
+}
+
+_execute_() {
+  # v1.0.1
+  local cmd="${1:?_execute_ needs a command}"
+  local message="${2:-$1}"
+  if ${dryrun}; then
+    dryrun "${message}"
+  else
+    if $verbose; then
+      eval "$cmd"
+    else
+      eval "$cmd" &> /dev/null
+    fi
+    if [ $? -eq 0 ]; then
+      success "${message}"
+    else
+      error "${message}"
+      #die "${message}"
+    fi
+  fi
+}
+
+# Set Base Variables
+# ----------------------
+scriptName=$(basename "$0")
+
+# Set Flags
+quiet=false;              printLog=false;             verbose=false;
+force=false;              strict=false;               dryrun=false;
+debug=false;              sourceOnly=false;           args=();
+
+# Set Colors
+bold=$(tput bold);        reset=$(tput sgr0);         purple=$(tput setaf 171);
+red=$(tput setaf 1);      green=$(tput setaf 76);     tan=$(tput setaf 3);
+blue=$(tput setaf 38);    underline=$(tput sgr 0 1);
+
+# Logging & Feedback
+logFile="${HOME}/Library/Logs/${scriptName%.sh}.log"
+
+_alert_() {
+  # v1.0.0
+  if [ "${1}" = "error" ]; then local color="${bold}${red}"; fi
+  if [ "${1}" = "warning" ]; then local color="${red}"; fi
+  if [ "${1}" = "success" ]; then local color="${green}"; fi
+  if [ "${1}" = "debug" ]; then local color="${purple}"; fi
+  if [ "${1}" = "header" ]; then local color="${bold}${tan}"; fi
+  if [ "${1}" = "input" ]; then local color="${bold}"; fi
+  if [ "${1}" = "dryrun" ]; then local color="${blue}"; fi
+  if [ "${1}" = "info" ] || [ "${1}" = "notice" ]; then local color=""; fi
+  # Don't use colors on pipes or non-recognized terminals
+  if [[ "${TERM}" != "xterm"* ]] || [ -t 1 ]; then color=""; reset=""; fi
+
+  # Print to console when script is not 'quiet'
+  if ${quiet}; then tput cuu1 ; return; else # tput cuu1 moves cursor up one line
+   echo -e "$(date +"%r") ${color}$(printf "[%7s]" "${1}") ${_message}${reset}";
+  fi
+
+  # Print to Logfile
+  if ${printLog} && [ "${1}" != "input" ]; then
+    color=""; reset="" # Don't use colors in logs
+    echo -e "$(date +"%m-%d-%Y %r") $(printf "[%7s]" "${1}") ${_message}" >> "${logFile}";
+  fi
+}
+
+function die ()       { local _message="${*} Exiting."; echo -e "$(_alert_ error)"; _safeExit_ "1";}
+function error ()     { local _message="${*}"; echo -e "$(_alert_ error)"; }
+function warning ()   { local _message="${*}"; echo -e "$(_alert_ warning)"; }
+function notice ()    { local _message="${*}"; echo -e "$(_alert_ notice)"; }
+function info ()      { local _message="${*}"; echo -e "$(_alert_ info)"; }
+function debug ()     { local _message="${*}"; echo -e "$(_alert_ debug)"; }
+function success ()   { local _message="${*}"; echo -e "$(_alert_ success)"; }
+function dryrun()     { local _message="${*}"; echo -e "$(_alert_ dryrun)"; }
+function input()      { local _message="${*}"; echo -n "$(_alert_ input)"; }
+function header()     { local _message="== ${*} ==  "; echo -e "$(_alert_ header)"; }
+function verbose()    { if ${verbose}; then debug "$@"; fi }
+
+
+# Options and Usage
+# -----------------------------------
+_usage_() {
+  echo -n "${scriptName} [OPTION]... [FILE]...
+
+This is a script template.  Edit this description to print help to users.
+
+ ${bold}Options:${reset}
+  --rootDIR         The location of the 'dotfiles' directory
+
+  -n, --dryrun      Non-destructive. Makes no permanent changes.
+  -q, --quiet       Quiet (no output)
+  -l, --log         Print log to file
+  -s, --strict      Exit script with null variables.  i.e 'set -o nounset'
+  -v, --verbose     Output more information. (Items echoed to 'verbose')
+  -d, --debug       Runs script in BASH debug mode (set -x)
+  -h, --help        Display this help and exit
+      --version     Output version information and exit
+      --source-only Bypasses main script functionality to allow unit tests of functions
+      --force       Skip all user interaction.  Implied 'Yes' to all actions.
+"
+}
+
+# Iterate over options breaking -ab into -a -b when needed and --foo=bar into
+# --foo bar
+optstring=h
+unset options
+while (($#)); do
+  case $1 in
+    # If option is of type -ab
+    -[!-]?*)
+      # Loop over each character starting with the second
+      for ((i=1; i < ${#1}; i++)); do
+        c=${1:i:1}
+
+        # Add current char to options
+        options+=("-$c")
+
+        # If option takes a required argument, and it's not the last char make
+        # the rest of the string its argument
+        if [[ $optstring = *"$c:"* && ${1:i+1} ]]; then
+          options+=("${1:i+1}")
+          break
+        fi
+      done
+      ;;
+
+    # If option is of type --foo=bar
+    --?*=*) options+=("${1%%=*}" "${1#*=}") ;;
+    # add --endopts for --
+    --) options+=(--endopts) ;;
+    # Otherwise, nothing special
+    *) options+=("$1") ;;
+  esac
+  shift
+done
+set -- "${options[@]}"
+unset options
+
+# Print help if no arguments were passed.
+# Uncomment to force arguments when invoking the script
+# -------------------------------------
+# [[ $# -eq 0 ]] && set -- "--help"
+
+# Read the options and set stuff
+while [[ $1 = -?* ]]; do
+  case $1 in
+    --rootDIR) shift; baseDir="$1" ;;
+    -h|--help) _usage_ >&2; _safeExit_ ;;
+    -n|--dryrun) dryrun=true ;;
+    -v|--verbose) verbose=true ;;
+    -l|--log) printLog=true ;;
+    -q|--quiet) quiet=true ;;
+    -s|--strict) strict=true;;
+    -d|--debug) debug=true;;
+    --version) echo "$(basename $0) ${version}"; _safeExit_ ;;
+    --source-only) sourceOnly=true;;
+    --force) force=true ;;
+    --endopts) shift; break ;;
+    *) die "invalid option: '$1'." ;;
+  esac
+  shift
+done
+
+# Store the remaining part as arguments.
+args+=("$@")
+
+# Trap bad exits with your cleanup function
+trap _trapCleanup_ EXIT INT TERM
+
+# Set IFS to preferred implementation
+IFS=$' \n\t'
+
+# Exit on error. Append '||true' when you run the script if you expect an error.
+# if using the 'execute' function this must be disabled for warnings to be shown if tasks fail
+#set -o errexit
+
+# Force pipelines to fail on the first non-zero status code.
+set -o pipefail
+
+# Run in debug mode, if set
+if ${debug}; then set -x ; fi
+
+# Exit on empty variable
+if ${strict}; then set -o nounset ; fi
+
+# Exit the script if a command fails
+#set -e
+
+# Run your script unless in 'source-only' mode
+if ! ${sourceOnly}; then _mainScript_; fi
+
+# Exit cleanly
+if ! ${sourceOnly}; then _safeExit_; fi
