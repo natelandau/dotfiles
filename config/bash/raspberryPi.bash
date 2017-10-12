5c8c0f1f0838e6cfd0b0a707c9c5a6928cb55580
Nathaniel Landau <nate@natelandau.com>
2017-10-11 23:40:24 -0400

    more settings

--- config/bash/raspberryPi.bash
+++ config/bash/raspberryPi.bash
@@ -4,6 +4,26 @@ _raspberryPi_ () {
   me=$(whoami)
 
   if [[ "$me" == "pi" ]]; then
+
+    # Fix raspberry pie locale issues
     export LC_ALL=C
+
+    # set variable identifying the chroot you work in (used in the prompt below)
+    if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+        debian_chroot=$(cat /etc/debian_chroot)
+    fi
+
+    # enable programmable completion features (you don't need to enable
+    # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+    # sources /etc/bash.bashrc).
+    if ! shopt -oq posix; then
+      if [ -f /usr/share/bash-completion/bash_completion ]; then
+        # shellcheck disable=1091
+        . /usr/share/bash-completion/bash_completion
+      elif [ -f /etc/bash_completion ]; then
+        # shellcheck disable=1091
+        . /etc/bash_completion
+      fi
+    fi
   fi
 }
