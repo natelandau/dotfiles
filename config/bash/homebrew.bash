adebf446d796d3a262829b012520c697fdee988c
Nathaniel Landau <nate@natelandau.com>
2017-11-03 10:32:32 -0400

    Updated brew cask handling

--- config/bash/homebrew.bash
+++ config/bash/homebrew.bash
@@ -1,5 +1,14 @@
 if command -v brew &>/dev/null ; then
-  alias bup='brew update && brew upgrade --all && brew cleanup && brew cask cleanup && brew prune'
   alias cask='brew cask'
   alias brwe='brew'  #typos
+
+bup() {
+  brew update
+  brew upgrade
+  [ -e "${HOME}/bin/upgradeCasks" ] && ${HOME}/bin/upgradeCasks
+  brew cleanup
+  brew cask cleanup
+  brew prune
+}
+
 fi
