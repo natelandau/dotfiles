67052147387c5f61b05aa02b20caff9f5d44c672
Nathaniel Landau <nate@natelandau.com>
2022-02-26 16:35:02 -0500

    Add 'llt' alias to filter files for today's date
    - Add llt alias to filter files for today's date
    - Remove --icons flag from exa when not available

--- shell/directories.sh
+++ shell/directories.sh
@@ -1,27 +1,40 @@
 # Different sets of LS aliases because Gnu LS and macOS LS use different
 # flags for colors.  Also, prefer gem colorls or exa when available.
 
-if command -v exa &>/dev/null; then
+if exa --icons &>/dev/null; then
     alias ls='exa --git --icons'
     alias l='exa --git --icons -lF'
     alias ll='exa -lahF --git'
     alias lll="exa -1F --git --icons"
     alias llm='ll --sort=modified'
-    alias la='ls -lbhHigUmuSa --color-scale'
-    alias lx='ls -lbhHigUmuSa@ --color-scale'
+    alias la='exa -lbhHigUmuSa --color-scale --git --icons'
+    alias lx='exa -lbhHigUmuSa@ --color-scale --git --icons'
     alias lt='exa --tree --level=2'
     alias llt='exa -lahF --tree --level=2'
-elif
-    command -v colorls &>/dev/null
-then
+    alias ltt='exa -lahF | grep "$(date +"%d %b")"'
+elif command -v exa &>/dev/null; then
+    alias ls='exa --git'
+    alias l='exa --git -lF'
+    alias ll='exa -lahF --git'
+    alias lll="exa -1F --git"
+    alias llm='ll --sort=modified'
+    alias la='exa -lbhHigUmuSa --color-scale --git'
+    alias lx='exa -lbhHigUmuSa@ --color-scale --git'
+    alias lt='exa --tree --level=2'
+    alias llt='exa -lahF --tree --level=2'
+    alias ltt='exa -lahF | grep "$(date +"%d %b")"'
+elif command -v colorls &>/dev/null; then
     alias ll="colorls -1A --git-status"
     alias ls="colorls -A"
+    alias ltt='colorls -A | grep "$(date +"%d %b")"'
 elif [[ $(command -v ls) =~ gnubin || $OSTYPE =~ linux ]]; then
     alias ls="ls --color=auto"
     alias ll='ls -FlAhpv --color=auto'
+    alias ltt='ls -FlAhpv| grep "$(date +"%d %b")"'
 else
     alias ls="ls -G"
     alias ll='ls -FGlAhpv'
+    alias ltt='ls -FlAhpv| grep "$(date +"%d %b")"'
 fi
 
 cd() {
