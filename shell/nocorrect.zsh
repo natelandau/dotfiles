49e859e011d157c11138e555708dd40723c3b5c6
Nathaniel Landau <nate@natelandau.com>
2022-06-27 08:58:01 -0400

    Fix zsh autocorrect for commands with associated dotfiles

---
+++ shell/nocorrect.zsh
@@ -0,0 +1,6 @@
+# ZSH autocorrect can be annoying when typing a command that has an associated dotfile.
+# These aliases disable autocorrect for specific commands.
+
+alias docker="nocorrect docker"
+alias nano="nocorrect nano"
+alias ssh="nocorrect ssh"
