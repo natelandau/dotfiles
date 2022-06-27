e4c33b2ae2032fd164f9c4b70b5e28c9593e8cab
Nathaniel Landau <nate@natelandau.com>
2022-06-26 22:43:27 -0400

    support pyenv

--- shell/python.sh
+++ shell/python.sh
@@ -1,3 +1,7 @@
+if ! command -v pyenv &>/dev/null && [ -d "${HOME}/.pyenv/bin" ]; then
+    export PATH="${HOME}/.pyenv/bin:${PATH}"
+fi
+
 if command -v pyenv &>/dev/null; then
     export PYENV_ROOT="${HOME}/.pyenv"
     export PATH="${PYENV_ROOT}/bin:${PATH}"
