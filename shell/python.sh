28c09a60bbe84f947cb373aae833b479071be0fb
Nathaniel Landau <nate@natelandau.com>
2022-07-11 18:18:16 -0400

    Add wrapper_no_poetry()

--- shell/python.sh
+++ shell/python.sh
@@ -4,3 +4,21 @@ if [ -d "${HOME}/.pyenv" ]; then
     eval "$(pyenv init --path)"
     eval "$(pyenv init -)"
 fi
+
+function wrapper_no_poetry() {
+    # Prefixing any command with wnp runs it outside the virtualenv if a virtualenv is active.
+    local last_env
+    if [[ -v VIRTUAL_ENV ]]; then
+        last_env="${VIRTUAL_ENV}"
+        deactivate
+    fi
+    "$@"
+    ret=$?
+    # shellcheck disable=SC1091
+    if [[ -v last_env ]]; then
+        . "${last_env}/bin/activate"
+    fi
+    return ${ret}
+}
+
+alias wnp='wrapper_no_poetry'
