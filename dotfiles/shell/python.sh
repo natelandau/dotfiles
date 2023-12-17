fcd9433d0b062c15b5c244e0d8f7f66473eb42bb
Nathaniel Landau <nate@natelandau.com>
2023-12-17 13:56:55 -0500

    fix: add `cz` and `test` to zsh nocorrect

--- dotfiles/shell/python.sh
+++ dotfiles/shell/python.sh
@@ -1,6 +1,7 @@
 if [ -d "${HOME}/.pyenv" ]; then
     export PYENV_ROOT="${HOME}/.pyenv"
-    command -v pyenv &>/dev/null || export PATH="${PYENV_ROOT}/bin:$PATH"
+
+    command -v pyenv >/dev/null || export PATH="${PYENV_ROOT}/bin:${PATH}"
     eval "$(pyenv init --path)"
     eval "$(pyenv init -)"
 fi
