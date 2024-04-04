7797539f7f454ab9dd0b62d3534487df87a00ba4
Nathaniel Landau <nate@natelandau.com>
2024-04-04 11:07:49 -0400

    fix: use most for viewing man pages

--- dotfiles/shell/bashOptions.bash
+++ dotfiles/shell/bashOptions.bash
@@ -6,7 +6,6 @@ if [[ ${SHELL##*/} == "bash" ]]; then
     export LANG="en_US"
     export LC_ALL="en_US.UTF-8"
     export LESS_TERMCAP_md="${yellow}" # Highlight section titles in manual pages
-    export MANPAGER="less -X"          # Don’t clear the screen after quitting a man page
     set -o noclobber                   # Prevent file overwrite on stdout redirection
     shopt -s checkwinsize              # Update window size after every command
     PROMPT_DIRTRIM=3                   # Automatically trim long paths in the prompt (requires Bash 4.x)
--- dotfiles/shell/colors.sh
+++ dotfiles/shell/colors.sh
@@ -34,13 +34,13 @@ export CLICOLOR=1
 #export LSCOLORS="ExFxBxDxCxegedabagacad"
 export LSCOLORS="GxFxCxDxCxegedabagaced"
 # LESS man page colors
-# export LESS_TERMCAP_mb=$'\E[01;31m'
-# export LESS_TERMCAP_md=$'\E[01;31m'
-# export LESS_TERMCAP_me=$'\E[0m'
-# export LESS_TERMCAP_se=$'\E[0m'
-# export LESS_TERMCAP_so=$'\E[01;44;33m'
-# export LESS_TERMCAP_ue=$'\E[0m'
-# export LESS_TERMCAP_us=$'\E[01;32m'
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
 
 if [[ $COLORTERM == gnome-* && $TERM == xterm ]] && infocmp gnome-256color >/dev/null 2>&1; then
     export TERM=gnome-256color
