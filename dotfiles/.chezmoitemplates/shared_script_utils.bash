e8f67f4ab99906cf12f17e0b185b25b704857f22
Nathaniel Landau <natelandau@users.noreply.github.com>
2024-05-05 17:30:48 -0400

    feat(macos): run macos config script (#11)

---
+++ dotfiles/.chezmoitemplates/shared_script_utils.bash
@@ -0,0 +1,323 @@
+# Set Defaults
+# ######################
+LOGFILE="${HOME}/logs/$(basename "$0").log"
+QUIET=false
+LOGLEVEL=ERROR
+VERBOSE=false
+FORCE=false
+DRYRUN=false
+
+# Set Options
+# ######################
+# Confirm we have BASH greater than v4
+[ "${BASH_VERSINFO:-0}" -ge 4 ] || {
+    shopt -s nullglob globstar # Make `for f in *.txt` work when `*.txt` matches zero files
+}
+
+trap '_trapCleanup_ ${LINENO} ${BASH_LINENO} "${BASH_COMMAND}" "${FUNCNAME[*]}" "${0}" "${BASH_SOURCE[0]}"' EXIT INT TERM SIGINT SIGQUIT SIGTERM
+set -o errtrace # Trap errors in subshells and functions
+set -o errexit  # Exit on error. Append '||true' if you expect an error
+set -o pipefail # Use last non-zero exit code in a pipeline
+IFS=$' \n\t'    # Set IFS to preferred implementation
+set -o nounset  # Disallow expansion of unset variables
+
+# Import functions
+# ######################
+_setColors_() {
+    # DESC:
+    #         Sets colors use for alerts.
+    # ARGS:
+    #         None
+    # OUTS:
+    #         None
+    # USAGE:
+    #         printf "%s\n" "${blue}Some text${reset}"
+
+    if tput setaf 1 >/dev/null 2>&1; then
+        bold=$(tput bold)
+        underline=$(tput smul)
+        reverse=$(tput rev)
+        reset=$(tput sgr0)
+
+        if [[ $(tput colors) -ge 256 ]] >/dev/null 2>&1; then
+            white=$(tput setaf 231)
+            blue=$(tput setaf 38)
+            yellow=$(tput setaf 11)
+            green=$(tput setaf 82)
+            red=$(tput setaf 9)
+            purple=$(tput setaf 171)
+            gray=$(tput setaf 250)
+        else
+            white=$(tput setaf 7)
+            blue=$(tput setaf 38)
+            yellow=$(tput setaf 3)
+            green=$(tput setaf 2)
+            red=$(tput setaf 9)
+            purple=$(tput setaf 13)
+            gray=$(tput setaf 7)
+        fi
+    else
+        bold="\033[4;37m"
+        reset="\033[0m"
+        underline="\033[4;37m"
+        # shellcheck disable=SC2034
+        reverse=""
+        white="\033[0;37m"
+        blue="\033[0;34m"
+        yellow="\033[0;33m"
+        green="\033[1;32m"
+        red="\033[0;31m"
+        purple="\033[0;35m"
+        gray="\033[0;37m"
+    fi
+}
+_setColors_
+
+_alert_() {
+    # DESC:
+    #         Controls all printing of messages to log files and stdout.
+    # ARGS:
+    #         $1 (required) - The type of alert to print
+    #                         (success, header, notice, dryrun, debug, warning, error,
+    #                         fatal, info, input)
+    #         $2 (required) - The message to be printed to stdout and/or a log file
+    #         $3 (optional) - Pass '${LINENO}' to print the line number where the _alert_ was triggered
+    # OUTS:
+    #         stdout: The message is printed to stdout
+    #         log file: The message is printed to a log file
+    # USAGE:
+    #         [_alertType] "[MESSAGE]" "${LINENO}"
+    # NOTES:
+    #         - The colors of each alert type are set in this function
+    #         - For specified alert types, the funcstac will be printed
+
+    local _color
+    local _alertType="${1}"
+    local _message="${2}"
+    local _line="${3:-}" # Optional line number
+
+    [[ $# -lt 2 ]] && fatal 'Missing required argument to _alert_'
+
+    if [[ -n ${_line} && ${_alertType} =~ ^(fatal|error) && ${FUNCNAME[2]} != "_trapCleanup_" ]]; then
+        _message="${_message} ${gray}(line: ${_line}) $(_printFuncStack_)"
+    elif [[ -n ${_line} && ${FUNCNAME[2]} != "_trapCleanup_" ]]; then
+        _message="${_message} ${gray}(line: ${_line})"
+    elif [[ -z ${_line} && ${_alertType} =~ ^(fatal|error) && ${FUNCNAME[2]} != "_trapCleanup_" ]]; then
+        _message="${_message} ${gray}$(_printFuncStack_)"
+    fi
+
+    if [[ ${_alertType} =~ ^(error|fatal) ]]; then
+        _color="${bold}${red}"
+    elif [ "${_alertType}" == "info" ]; then
+        _color="${gray}"
+    elif [ "${_alertType}" == "warning" ]; then
+        _color="${red}"
+    elif [ "${_alertType}" == "success" ]; then
+        _color="${green}"
+    elif [ "${_alertType}" == "debug" ]; then
+        _color="${purple}"
+    elif [ "${_alertType}" == "header" ]; then
+        _color="${bold}${white}${underline}"
+    elif [ "${_alertType}" == "notice" ]; then
+        _color="${bold}"
+    elif [ "${_alertType}" == "input" ]; then
+        _color="${bold}${underline}"
+    elif [ "${_alertType}" = "dryrun" ]; then
+        _color="${blue}"
+    else
+        _color=""
+    fi
+
+    _writeToScreen_() {
+        ("${QUIET}") && return 0 # Print to console when script is not 'quiet'
+        [[ ${VERBOSE} == false && ${_alertType} =~ ^(debug|verbose) ]] && return 0
+
+        if ! [[ -t 1 || -z ${TERM:-} ]]; then # Don't use colors on non-recognized terminals
+            _color=""
+            reset=""
+        fi
+
+        if [[ ${_alertType} == header ]]; then
+            printf "${_color}%s${reset}\n" "${_message}"
+        else
+            printf "${_color}[%7s] %s${reset}\n" "${_alertType}" "${_message}"
+        fi
+    }
+    _writeToScreen_
+
+    _writeToLog_() {
+        [[ ${_alertType} == "input" ]] && return 0
+        [[ ${LOGLEVEL} =~ (off|OFF|Off) ]] && return 0
+        if [ -z "${LOGFILE:-}" ]; then
+            LOGFILE="$(pwd)/$(basename "$0").log"
+        fi
+        [ ! -d "$(dirname "${LOGFILE}")" ] && mkdir -p "$(dirname "${LOGFILE}")"
+        [[ ! -f ${LOGFILE} ]] && touch "${LOGFILE}"
+
+        # Don't use colors in logs
+        local _cleanmessage
+        _cleanmessage="$(printf "%s" "${_message}" | sed -E 's/(\x1b)?\[(([0-9]{1,2})(;[0-9]{1,3}){0,2})?[mGK]//g')"
+        # Print message to log file
+        printf "%s [%7s] %s %s\n" "$(date +"%b %d %R:%S")" "${_alertType}" "[$(/bin/hostname)]" "${_cleanmessage}" >>"${LOGFILE}"
+    }
+
+    # Write specified log level data to logfile
+    case "${LOGLEVEL:-ERROR}" in
+        ALL | all | All)
+            _writeToLog_
+            ;;
+        DEBUG | debug | Debug)
+            _writeToLog_
+            ;;
+        INFO | info | Info)
+            if [[ ${_alertType} =~ ^(error|fatal|warning|info|notice|success) ]]; then
+                _writeToLog_
+            fi
+            ;;
+        NOTICE | notice | Notice)
+            if [[ ${_alertType} =~ ^(error|fatal|warning|notice|success) ]]; then
+                _writeToLog_
+            fi
+            ;;
+        WARN | warn | Warn)
+            if [[ ${_alertType} =~ ^(error|fatal|warning) ]]; then
+                _writeToLog_
+            fi
+            ;;
+        ERROR | error | Error)
+            if [[ ${_alertType} =~ ^(error|fatal) ]]; then
+                _writeToLog_
+            fi
+            ;;
+        FATAL | fatal | Fatal)
+            if [[ ${_alertType} =~ ^fatal ]]; then
+                _writeToLog_
+            fi
+            ;;
+        OFF | off)
+            return 0
+            ;;
+        *)
+            if [[ ${_alertType} =~ ^(error|fatal) ]]; then
+                _writeToLog_
+            fi
+            ;;
+    esac
+
+} # /_alert_
+
+error() { _alert_ error "${1}" "${2:-}"; }
+warning() { _alert_ warning "${1}" "${2:-}"; }
+notice() { _alert_ notice "${1}" "${2:-}"; }
+info() { _alert_ info "${1}" "${2:-}"; }
+success() { _alert_ success "${1}" "${2:-}"; }
+dryrun() { _alert_ dryrun "${1}" "${2:-}"; }
+input() { _alert_ input "${1}" "${2:-}"; }
+header() { _alert_ header "${1}" "${2:-}"; }
+debug() { _alert_ debug "${1}" "${2:-}"; }
+fatal() {
+    _alert_ fatal "${1}" "${2:-}"
+    _safeExit_ "1"
+}
+
+_printFuncStack_() {
+    # DESC:
+    #         Prints the function stack in use. Used for debugging, and error reporting.
+    # ARGS:
+    #         None
+    # OUTS:
+    #         stdout: Prints [function]:[file]:[line]
+    # NOTE:
+    #         Does not print functions from the alert class
+    local _i
+    declare -a _funcStackResponse=()
+    for ((_i = 1; _i < ${#BASH_SOURCE[@]}; _i++)); do
+        case "${FUNCNAME[${_i}]}" in
+            _alert_ | _trapCleanup_ | fatal | error | warning | notice | info | debug | dryrun | header | success)
+                continue
+                ;;
+            *)
+                _funcStackResponse+=("${FUNCNAME[${_i}]}:$(basename "${BASH_SOURCE[${_i}]}"):${BASH_LINENO[_i - 1]}")
+                ;;
+        esac
+
+    done
+    printf "( "
+    printf %s "${_funcStackResponse[0]}"
+    printf ' < %s' "${_funcStackResponse[@]:1}"
+    printf ' )\n'
+}
+
+_safeExit_() {
+    # DESC:
+    #       Cleanup and exit from a script
+    # ARGS:
+    #       $1 (optional) - Exit code (defaults to 0)
+    # OUTS:
+    #       None
+
+    if [[ -d ${SCRIPT_LOCK:-} ]]; then
+        if command rm -rf "${SCRIPT_LOCK}"; then
+            debug "Removing script lock"
+        else
+            warning "Script lock could not be removed. Try manually deleting ${yellow}'${SCRIPT_LOCK}'"
+        fi
+    fi
+
+    if [[ -n ${TMP_DIR:-} && -d ${TMP_DIR:-} ]]; then
+        if [[ ${1:-} == 1 && -n "$(ls "${TMP_DIR}")" ]]; then
+            command rm -r "${TMP_DIR}"
+        else
+            command rm -r "${TMP_DIR}"
+            debug "Removing temp directory"
+        fi
+    fi
+
+    trap - INT TERM EXIT
+    exit "${1:-0}"
+}
+
+_trapCleanup_() {
+    # DESC:
+    #         Log errors and cleanup from script when an error is trapped.  Called by 'trap'
+    # ARGS:
+    #         $1:  Line number where error was trapped
+    #         $2:  Line number in function
+    #         $3:  Command executing at the time of the trap
+    #         $4:  Names of all shell functions currently in the execution call stack
+    #         $5:  Scriptname
+    #         $6:  $BASH_SOURCE
+    # USAGE:
+    #         trap '_trapCleanup_ ${LINENO} ${BASH_LINENO} "${BASH_COMMAND}" "${FUNCNAME[*]}" "${0}" "${BASH_SOURCE[0]}"' EXIT INT TERM SIGINT SIGQUIT SIGTERM ERR
+    # OUTS:
+    #         Exits script with error code 1
+
+    local _line=${1:-} # LINENO
+    local _linecallfunc=${2:-}
+    local _command="${3:-}"
+    local _funcstack="${4:-}"
+    local _script="${5:-}"
+    local _sourced="${6:-}"
+
+    # Replace the cursor in-case 'tput civis' has been used
+    tput cnorm
+
+    if declare -f "fatal" &>/dev/null && declare -f "_printFuncStack_" &>/dev/null; then
+
+        _funcstack="'$(printf "%s" "${_funcstack}" | sed -E 's/ / < /g')'"
+
+        if [[ ${_script##*/} == "${_sourced##*/}" ]]; then
+            fatal "${7:-} command: '${_command}' (line: ${_line}) [func: $(_printFuncStack_)]"
+        else
+            fatal "${7:-} command: '${_command}' (func: ${_funcstack} called at line ${_linecallfunc} of '${_script##*/}') (line: ${_line} of '${_sourced##*/}') "
+        fi
+    else
+        printf "%s\n" "Fatal error trapped. Exiting..."
+    fi
+
+    if declare -f _safeExit_ &>/dev/null; then
+        _safeExit_ 1
+    else
+        exit 1
+    fi
+}
