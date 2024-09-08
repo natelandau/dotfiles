3179fe249c284f88405d2168abe560de4bb90628
Nathaniel Landau <nate@natelandau.com>
2024-09-08 19:21:47 -0400

    fix: consolidate changes to PATh

---
+++ dotfiles/dot_shell/sourced/0040-path.sh
@@ -0,0 +1,17 @@
+# Build PATH
+_myPaths=(
+    "${HOME}/.local/bin"
+    "/usr/local/bin"
+    "/opt/homebrew/bin"
+    "${HOME}/bin"
+    "${HOME}/.cargo/bin"
+    "/usr/bin/snap"
+)
+
+for _path in "${_myPaths[@]}"; do
+    if [[ -d ${_path} ]]; then
+        if ! printf "%s" "${_path}" | grep -q "${PATH}"; then
+            PATH="${_path}:${PATH}"
+        fi
+    fi
+done
