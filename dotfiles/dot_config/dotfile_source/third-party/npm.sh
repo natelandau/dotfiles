fe9ea64d178fe368dc9bdc59949d204b6f2bdfee
Nathaniel Landau <nate@natelandau.com>
2026-01-28 22:28:29 -0500

    feat(node): add support for nvm and pnpm

--- dotfiles/dot_config/dotfile_source/third-party/npm.sh
+++
@@ -1,9 +0,0 @@
-# Run locally installed npm packages
-# http://stackoverflow.com/questions/9679932/how-to-use-package-installed-locally-in-node-modules
-# usage:
-#   $ npm-exec grunt
-
-if command -v npm &>/dev/null; then
-    alias npm-exec='PATH=$(npm bin):$PATH'
-    alias grunt="npm-exec grunt"
-fi
